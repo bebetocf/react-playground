56ec2bab9259c6e1211a8ecd09f466665d424159
Roberto <betocostaf@gmail.com>
2020-04-17 20:26:08 -0300

    dynamic css using modules

--- rcg/sub-01/src/App.js
+++ rcg/sub-01/src/App.js
@@ -1,22 +1,7 @@
 import React, { Component } from 'react';
-import styled from 'styled-components'
 
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person'
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-  
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-    color: black;
-  }
-`
 
 class App extends Component {
   state = {
@@ -33,8 +18,6 @@ class App extends Component {
     ],
     showPersons: false
   }
-
-  
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
@@ -64,6 +47,7 @@ class App extends Component {
 
   render() {
     let persons = null;
+    let btnClasses = null;
 
     if (this.state.showPersons) {
       persons = (
@@ -81,34 +65,30 @@ class App extends Component {
           })}
         </div>
         );
-        // buttonStyle.backgroundColor = 'red';
-        // buttonStyle[':hover'] = {
-        //   backgroundColor: 'salmon',
-        //   color: 'black'
-        // }
+
+        btnClasses = classes.Red;
     }
 
-    const classes = []
+    const assignedClasses = []
     if (this.state.persons.length <= 2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1){
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hello World</h1> 
-        <p className={classes.join(' ')} > This is working </p>
-        <StyledButton 
-          alt={this.state.showPersons}
+        <p className={assignedClasses.join(' ')} > This is working </p>
+        <button 
+          className={btnClasses}
           onClick={this.togglePersonsHandler}>
             Toggle Persons
-          </StyledButton>
+          </button>
         {persons}
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello World'))
   }
 }
 
