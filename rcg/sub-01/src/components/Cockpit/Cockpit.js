22d3f181eb21270970f036f7fbd1693127f892a1
Roberto Fernandes <betocostaf@gmail.com>
2020-04-19 16:20:40 -0300

    adding eslint and prettier

--- rcg/sub-01/src/components/Cockpit/Cockpit.js
+++ rcg/sub-01/src/components/Cockpit/Cockpit.js
@@ -1,33 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-import classes from './Cockpit.css'
+import classes from './Cockpit.css';
 
 const Cockpit = (props) => {
-    const assignedClasses = []
-    let btnClasses = null;
+  const assignedClasses = [];
+  let btnClasses = null;
 
-    if (props.showPersons) {
-        btnClasses = classes.Red;
-    }
+  if (props.showPersons) {
+    btnClasses = classes.Red;
+  }
 
-    if (props.persons.length <= 2){
-      assignedClasses.push(classes.red);
-    }
-    if (props.persons.length <= 1){
-      assignedClasses.push(classes.bold);
-    }
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red);
+  }
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold);
+  }
 
-    return (
-        <div className={classes.Cockpit} >
-            <h1>Hello World</h1> 
-            <p className={assignedClasses.join(' ')} > This is working </p>
-            <button 
-                className={btnClasses}
-                onClick={props.clicked}>
-                    Toggle Persons
-            </button>
-        </div>
-    );
-}
+  return (
+    <div className={classes.Cockpit}>
+      <h1>Hello World</h1>
+      <p className={assignedClasses.join(' ')}> This is working </p>
+      <button className={btnClasses} onClick={props.clicked}>
+        Toggle Persons
+      </button>
+    </div>
+  );
+};
 
 export default Cockpit;
--- rcg/sub-01/src/containers/App.js
+++ rcg/sub-01/src/containers/App.js
@@ -1,68 +1,77 @@
 import React, { Component } from 'react';
 
 import classes from './App.css';
-import Persons from '../components/Persons/Persons'
-import Cockpit from '../components/Cockpit/Cockpit'
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
   state = {
     persons: [
       {
-        name: 'Max', age: 28, id: 'adadcacd'
+        name: 'Max',
+        age: 28,
+        id: 'adadcacd',
       },
       {
-        name: 'Bob', age: 23, id: 'gebsvsdc'
+        name: 'Bob',
+        age: 23,
+        id: 'gebsvsdc',
       },
       {
-        name: 'Tony', age: 17, id: 'faffefra'
-      }
+        name: 'Tony',
+        age: 17,
+        id: 'faffefra',
+      },
     ],
-    showPersons: false
-  }
+    showPersons: false,
+  };
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
-  }
+    this.setState({ showPersons: !doesShow });
+  };
 
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice();
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    this.setState({persons: persons});
-  }
+    this.setState({ persons: persons });
+  };
 
   changeNameHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
+    const personIndex = this.state.persons.findIndex((p) => {
       return p.id === id;
     });
 
-    const person = {...this.state.persons[personIndex]};
+    const person = { ...this.state.persons[personIndex] };
     person.name = event.target.value;
     const persons = [...this.state.persons];
     persons[personIndex] = person;
     this.setState({
-      persons: persons
-    })
-  }
+      persons: persons,
+    });
+  };
 
   render() {
     let persons = null;
 
     if (this.state.showPersons) {
-      persons = <Persons
-            persons={this.state.persons}
-            clicked={this.deletePersonHandler}
-            changed={this.changeNameHandler} /> ;
-
+      persons = (
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.changeNameHandler}
+        />
+      );
     }
 
     return (
       <div className={classes.App}>
-        <Cockpit 
+        <Cockpit
           showPersons={this.state.showPersons}
           persons={this.state.showPersons}
-          clicked={this.togglePersonsHandler} />
+          clicked={this.togglePersonsHandler}
+        />
         {persons}
       </div>
     );
