050af55658bff5ddaf73237ac1486a2f4f47f853
Roberto <betocostaf@gmail.com>
2020-04-21 18:45:44 -0300

    Adding aux component

--- rcg/sub-01/src/components/Persons/Person/Person.js
+++ rcg/sub-01/src/components/Persons/Person/Person.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 
 import classes from "./Person.css";
+import Aux from "../../../hoc/Aux";
 
 class Person extends Component {
   render() {
     console.log("[Person.js] rendering...");
     return (
-      // <div className="Person" style={personStyle} >
-      <div className={classes.Person}>
+      // <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I'm {this.props.age} years old!
         </p>
@@ -17,7 +18,8 @@ class Person extends Component {
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </div>
+      </Aux>
+      // </div>
     );
   }
 }
