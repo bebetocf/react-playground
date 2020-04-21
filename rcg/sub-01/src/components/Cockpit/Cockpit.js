9c2cb1068a827053647364910923b138d1bad2a0
Roberto Fernandes <betocostaf@gmail.com>
2020-04-21 14:37:48 -0300

    cockpit useEffect

--- rcg/sub-01/src/components/Cockpit/Cockpit.js
+++ rcg/sub-01/src/components/Cockpit/Cockpit.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import classes from "./Cockpit.css";
 
 const Cockpit = (props) => {
+  useEffect(() => {
+    console.log("[Cockpit] useEffect");
+  });
+
   const assignedClasses = [];
   let btnClasses = null;
 
