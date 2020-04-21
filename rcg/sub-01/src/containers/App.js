6a86bf10e04a458ed381ee3b10f6e4d8ea002cce
Roberto <betocostaf@gmail.com>
2020-04-21 12:32:30 -0300

    persons and person class

--- rcg/sub-01/src/containers/App.js
+++ rcg/sub-01/src/containers/App.js
@@ -40,6 +40,15 @@ class App extends Component {
     console.log("[App.js] componentDidMount");
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[App.js] shouldComponentUpdate");
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log("[App.js] componentDidUpdate");
+  }
+
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({ showPersons: !doesShow });
