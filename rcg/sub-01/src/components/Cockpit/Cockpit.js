aede04dc3114bac1fe7c168746c3298009bdb0d7
Roberto <betocostaf@gmail.com>
2020-04-21 21:02:15 -0300

    authentication context

--- rcg/sub-01/src/components/Cockpit/Cockpit.js
+++ rcg/sub-01/src/components/Cockpit/Cockpit.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 
 import classes from "./Cockpit.css";
+import AuthContext from "../../context/auth-context";
 
 const Cockpit = (props) => {
   const toggleBtnRef = useRef(null);
@@ -45,7 +46,9 @@ const Cockpit = (props) => {
       <button ref={toggleBtnRef} className={btnClasses} onClick={props.clicked}>
         Toggle Persons
       </button>
-      <button onClick={props.login}>Log in</button>
+      <AuthContext.Consumer>
+        {(context) => <button onClick={context.login}>Log in</button>}
+      </AuthContext.Consumer>
     </div>
   );
 };
--- rcg/sub-01/src/components/Persons/Person/Person.js
+++ rcg/sub-01/src/components/Persons/Person/Person.js
@@ -1,9 +1,10 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 import classes from "./Person.css";
 import Aux from "../../../hoc/Aux";
 import withClass from "../../../hoc/withClass";
+import AuthContext from "../../../context/auth-context";
 
 class Person extends Component {
   constructor(props) {
@@ -20,7 +21,11 @@ class Person extends Component {
     console.log("[Person.js] rendering...");
     return (
       <Aux>
-        {this.props.isAuth ? <p>Authenticated</p> : <p>Please Login</p>}
+        <AuthContext.Consumer>
+          {(context) =>
+            context.authenticated ? <p>Authenticated</p> : <p>Please Login</p>
+          }
+        </AuthContext.Consumer>
         <p onClick={this.props.click}>
           I'm {this.props.name} and I'm {this.props.age} years old!
         </p>
