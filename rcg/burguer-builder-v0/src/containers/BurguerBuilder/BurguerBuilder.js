616aa75c396f115450b7e9097c13d5e175cb439f
Roberto Fernandes <betocostaf@gmail.com>
2020-04-27 12:50:04 -0300

    adding more click listener

--- rcg/burguer-builder-v0/src/containers/BurguerBuilder/BurguerBuilder.js
+++ rcg/burguer-builder-v0/src/containers/BurguerBuilder/BurguerBuilder.js
@@ -4,6 +4,13 @@ import Aux from "../../hoc/Aux";
 import Burguer from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
+const INGREDIENTS_PRICE = {
+  salad: 0.3,
+  bacon: 0.4,
+  cheese: 0.7,
+  meat: 1.4,
+};
+
 class BurguerBuilder extends Component {
   state = {
     ingredients: {
@@ -12,12 +19,24 @@ class BurguerBuilder extends Component {
       cheese: 0,
       meat: 0,
     },
+    totalPrice: 4,
   };
+
+  addIngredientHandler = (type) => {
+    const updatedIngredients = { ...this.state.ingredients };
+    updatedIngredients[type] += 1;
+    const updatedPrice = this.state.totalPrice + INGREDIENTS_PRICE[type];
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: updatedPrice,
+    });
+  };
+
   render() {
     return (
       <Aux>
         <Burguer ingredients={this.state.ingredients} />
-        <BuildControls />
+        <BuildControls ingredientsAdd={this.addIngredientHandler} />
       </Aux>
     );
   }
